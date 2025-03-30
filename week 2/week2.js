543f7c9de9ba06a5130d0db405c19c79d9285055
anishafifza <b122410321@student.utem.edu.my>
2025-03-30 22:34:48 +0800

    Updated

--- week 2/week2.js
+++ week 2/week2.js
@@ -26,30 +26,46 @@ drivers.push({
     name: "David",
     vehicleType: "Truck",
     isAvailable: true,
-    rating: 4.8
+    rating: 4.6
 });
 
-async function main() {
+async function main() 
+{
     const uri = "mongodb://localhost:27017/"
     const client = new MongoClient(uri);
 
     try {
-        await driversCollection.connect();
+        await client.connect();
         const db = client.db("testDB");
        
-        const driversCollection = db.collection(`drivers`);
+        const driversCollection = db.collection("drivers"); // Insert Drivers into MongoDB
 
-        for (const driver of drivers) {
-            const result = await driversCollection.insertOne(driver);
-            console.log(`New driver created with result: ${result.insertedId}`);
+        // for (const driver of drivers) {
+        //     const result = await driversCollection.insertOne(driver);
+        //     console.log(`New driver created with result: ${result.insertedId}`);
+        // }
+        
+        // Query and Update Drivers
+        // const availableDrivers = await db.collection('drivers').find(
+        //     {
+        //         isAvailable: true,
+        //         rating: { $gte: 4.5 }
+        //     }).toArray();
+        //     console.log("Available drivers:", availableDrivers);
+
+        // Update
+        // const updateResult = await db.collection('drivers').updateOne(
+        //     { name: "John Doe" },
+        //     { $inc: { rating: 0.1 } }
+        // );
+        // console.log(`Drivers updated with result: ${updateResult}`);
+
+        // Delete - Task 6
+        const deleteResult = await db.collection('drivers').deleteOne({ isAvailable: false });
+        console.log(`Driver delete with result: ${deleteResult}`);
         }
-    } 
-
-    catch(err) {
-        console.error("Error:", err);
-    }
-
-    finally {
+    finally 
+    {
         await client.close();
     }
 }
