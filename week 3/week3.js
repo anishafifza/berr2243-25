47ba1a4eb2eb902aebea3c35fa21e32eb0c35337
anishafifza <b122410321@student.utem.edu.my>
2025-04-09 21:40:29 +0800

    Initial setup: Setup documentation

---
+++ week 3/week3.js
@@ -0,0 +1,34 @@
+// import the MongoClient class from the MongoDB library
+// install with "npm install mongodb"
+// function node is run to code "node index.js"
+
+const { MongoClient, Collection } = require('mongodb');
+
+async function main() {
+    // Replace <connection-string> with your MongDB URI
+    const uri = "mongodb://localhost:27017/"
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const db = client.db("testDB");
+        const collection = db.collection("users");
+
+
+        // Insert a document
+        await collection.insertOne({ name: "Anis", age: 23 });
+        console.log("Document inserted");
+
+        // Query the document
+        const result = await collection.findOne({ name: "Anis"});
+        console.log("Query result:", result);
+    } catch (err) {
+        console.error("Error:", err);
+    } finally {
+        await client.close();
+    }
+}
+
+main();
